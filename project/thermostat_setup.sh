6cafd1f6fa80c955103c6232254cc74c5d3f18e8
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-10-01 22:41:29 -0400

    Update thermostat_setup.sh

--- project/thermostat_setup.sh
+++ project/thermostat_setup.sh
@@ -4,3 +4,8 @@ sudo apt-get python3-pandas
 pip3 install pyowm
 pip3 install geocoder
 echo "All python dependencies have been successfully installed."
+
+mkdir ~/cassandra
+cd cassandra
+# https://academy.datastax.com/all-downloads?field_download_driver_category_tid=910
+wget ""
