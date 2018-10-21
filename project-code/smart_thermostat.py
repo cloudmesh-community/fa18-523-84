cd21e4d53f47d027c4553481960a38c8f9e18b40
ahilgenkamp <ahilgie@gmail.com>
2018-10-21 12:39:34 -0400

    Updating smart_thermostat code to include changes made for raspberry pi

--- project-code/smart_thermostat.py
+++ project-code/smart_thermostat.py
@@ -1,3 +1,4 @@
+
 # I523 Course Project
 # Smart Thermostat
 
@@ -12,29 +13,35 @@ import datetime
 #import pytz
 #from tzwhere import tzwhere
 from cassandra.cluster import Cluster
+from RPLCD import CharLCD
+from RPi import GPIO
 
 ######################
 # function to collect Weather Data
 # Sources for this section:
 # 	documentation for the open weather api python module: https://pyowm.readthedocs.io/en/latest/usage-examples.html#create-global-owm-object
 # 	geocoder code copied from stackoverflow post by Apollo_LFB: https://stackoverflow.com/questions/24906833/get-your-location-through-python
-# 	geocoder docs also used to understand the package usage: https://geocoder.readthedocs.io/ 
+# 	geocoder docs also used to understand the package usage: https://geocoder.readthedocs.io/
 ######################
-def get_current_weather():
-	g = geocoder.ip('me')
-	owm = pyowm.OWM('c38dcf6008303a9e9ff6464a5850e3ef')
+
+g = geocoder.ip('me')
+
+def get_current_weather(g):
+	owm = pyowm.OWM('24a35d03fca238fc68c5a56406696e20')
 	obs = owm.weather_at_coords(g.latlng[0],g.latlng[1])
 	w = obs.get_weather()
 	curr_weather_data = [w.get_reference_time(timeformat='date'), w.get_detailed_status(), w.get_temperature('fahrenheit')['temp']]
-	#curr_weather_df = pd.DataFrame([curr_weather_data], columns = ['Date and Time','Condition','Temp (F)'])
 	return curr_weather_data
 
 
 ######################
-# functions to get temperature data
+# functions to get temperature data and LCD setup
 # Sources for this section:
 #   code in this section copied from:  http://www.circuitbasics.com/raspberry-pi-ds18b20-temperature-sensor-tutorial/ 
 ######################
+
+GPIO.setwarnings(False)
+lcd = CharLCD(cols=16,rows=2,pin_rs=37,pin_e=35,pins_data=[33,31,29,23],numbering_mode=GPIO.BOARD)
 
 def read_temp_raw():
 	os.system('modprobe w1-gpio')
@@ -49,16 +56,19 @@ def read_temp_raw():
 
 
 def read_temp():
-	lines = read_temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
+	try:
 		lines = read_temp_raw()
+		while lines[0].strip()[-3:] != 'YES':
+			time.sleep(0.2)
+			lines = read_temp_raw()
 		equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		temp_c = float(temp_string) / 1000.0
-		temp_f = temp_c * 9.0 / 5.0 + 32.0
-	return temp_c, temp_f
+		if equals_pos != -1:
+			temp_string = lines[1][equals_pos+2:]
+			temp_c = float(temp_string) / 1000.0
+			temp_f = temp_c * 9.0 / 5.0 + 32.0
+		return temp_c, temp_f
+	except:
+		return 0, 0
 
 
 ######################
@@ -87,37 +97,47 @@ def cassandra_query(keyspace, query, params=(), return_data=False, contact_point
 			session = cluster.connect( keyspace )
 			session.execute( query, params )
 			cluster.shutdown()
-			return 'Data loaded to database'
-	except AttributeError:
-		raise
+	except:
+		print('Data not loaded. Check for Error')
 
 
 ######################
 # Output
 ######################
 
-now = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
-timeStampVal = get_current_weather()[0] - datetime.timedelta(hours=4) #convert to EST: Need to find a way to convert dynamically
-condition = get_current_weather()[1]
-out_temp_f = get_current_weather()[2]
-in_temp_f = 71.2 
-#in_temp_c, in_temp_f = read_temp()
-
-insert_data = '''
-            INSERT INTO temp_data (indoor_time, outdoor_time, out_condition, out_temp_f, in_temp_f)
-            VALUES (%s,%s,%s,%s,%s)
-            ''' 
-
-params = (now,str(timeStampVal),condition,out_temp_f,in_temp_f)
-
-print(cassandra_query('environment_data', insert_data, params))
-
-
-#print(read_temp())
-
 #g = geocoder.ip('me')
 #print(g.geojson)
 
-#while True:
-#  print(read_temp())  
-#  time.sleep(1)
+while True:
+	delay = 40
+	while delay > 0:
+
+		try:
+			curr_weather = get_current_weather(g)
+
+			now = datetime.datetime.utcnow() - datetime.timedelta(hours=4)
+			timeStampVal = curr_weather[0] - datetime.timedelta(hours=4) #convert to EST: Need to find a way to convert dynamically
+			condition = curr_weather[1]
+			out_temp_f = curr_weather[2]
+			#in_temp_f = 71.2
+			in_temp_c, in_temp_f = read_temp()
+
+			insert_data = '''
+		            INSERT INTO temp_data (indoor_time, outdoor_time, out_condition, out_temp_f, in_temp_f)
+	        	    VALUES (%s,%s,%s,%s,%s)
+		            '''
+
+			params = (now,str(timeStampVal),condition,out_temp_f,in_temp_f)
+
+			cassandra_query('environment_data', insert_data, params)
+
+			lcd.cursor_pos = (0,0)
+			lcd.write_string('Indoor: ' + str(round(in_temp_f,1)) + 'F')
+			lcd.cursor_pos = (1,0)
+			lcd.write_string('Outdoor: ' + str(round(out_temp_f,1)) + 'F')
+
+			time.sleep(15)
+			delay = delay - 1
+		except:
+			print('Error, no data loaded. Time: '+str(datetime.datetime.utcnow()-datetime.timedelta(hours=4)))
+	print(str(now)+' | '+str(in_temp_f)+' | '+str(out_temp_f)+' | '+str(timeStampVal))
