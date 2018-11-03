20f297c300ce7728013b3044d0e155b3b1f72c2f
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-02 23:26:18 -0400

    Update test_display.py

--- project-code/dev_code/test_display.py
+++ project-code/dev_code/test_display.py
@@ -1,3 +1,5 @@
+# Source: http://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
+
 from RPLCD import CharLCD
 from RPi import GPIO
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35,pins_data=[33,31,29,23], numbering_mode=GPIO.BOARD)
