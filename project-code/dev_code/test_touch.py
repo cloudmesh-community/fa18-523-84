a2d8e8227b6a801c6bc59a924d7355a46a685482
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-04 09:38:44 -0500

    Update test_touch.py

--- project-code/dev_code/test_touch.py
+++ project-code/dev_code/test_touch.py
@@ -3,16 +3,26 @@
 import RPi.GPIO as GPIO
 import time
 
-touch_pin = 13
+TOUCH_PIN = 13
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(touch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(TOUCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def touch_callback(channel):
+	print('Hey!')
+
+GPIO.add_event_detect(TOUCH_PIN, GPIO.RISING, callback=touch_callback)
 
 while True:
-	if GPIO.input(touch_pin) == 1:
+	time.sleep(1)
+
+'''
+while True:
+	if GPIO.input(TOUCH_PIN) == 1:
 		print('HEY')
 	else:
 		pass
-	#print(GPIO.input(touch_pin))
+	#print(GPIO.input(TOUCH_PIN))
 	time.sleep(1)
+'''
