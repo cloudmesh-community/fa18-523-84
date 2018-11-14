1a94f3e8c11dc30b79a282af702e43b5bc61cd7d
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-14 08:35:05 -0500

    Create touch_sensor.py

---
+++ paper/code/touch_sensor.py
@@ -0,0 +1,31 @@
+# Source: https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+# Source2: https://stackoverflow.com/questions/16143842/raspberry-pi-gpio-events-in-python
+
+import RPi.GPIO as GPIO
+import time
+
+TOUCH_PIN = 13
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(TOUCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+display_num = 1
+
+def touch_callback(channel):
+	global display_num
+	if display_num == 1:
+		if GPIO.input(TOUCH_PIN) == 1:
+			display_num = 0
+		else:
+			display_num = 1
+	else:
+		if GPIO.input(TOUCH_PIN) == 1:
+			display_num = 1
+		else:
+			display_num = 0
+
+GPIO.add_event_detect(TOUCH_PIN, GPIO.RISING, callback=touch_callback)
+
+while True:
+	time.sleep(5)
+	print(display_num)
