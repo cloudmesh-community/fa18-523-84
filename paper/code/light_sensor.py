7f601bed7550b1ae2c296688693611b5fa7fb1de
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-14 08:05:18 -0500

    Update light_sensor.py

--- paper/code/light_sensor.py
+++ paper/code/light_sensor.py
@@ -7,16 +7,15 @@ import time
 class READ_LIGHT_SENSOR(object):
 	"""Pin numbers for READ_LIGHT_SENSOR are based on BOARD numbers by default"""
 	def __init__(self, pin=7, pin_setup='BOARD'):
+		self.pin = pin
 		if pin_setup == 'BCM':
 			GPIO.setmode(GPIO.BCM)
 		else:
 			GPIO.setmode(GPIO.BOARD)
 		GPIO.setwarnings(False)
-		GPIO.setup(pin,GPIO.IN)
-		self.pin = pin
-		
+		GPIO.setup(self.pin,GPIO.IN)
+			
 	def return_data(self, pin):
-		print( GPIO.input(self.pin) )
 		return GPIO.input(self.pin)
 
 while True:
