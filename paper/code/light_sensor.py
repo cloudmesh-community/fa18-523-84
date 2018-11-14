d04cf6e05efc5b057fc507e2ecd958e30560da8e
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-14 07:46:45 -0500

    Create light_sensor.py

---
+++ paper/code/light_sensor.py
@@ -0,0 +1,19 @@
+# Source: www.uugear.com/portfolio/using-light-sensor-module-with-raspberry-pi/
+
+import RPi.GPIO as GPIO
+import time
+
+class READ_LIGHT_SENSOR(object):
+	"""Pin numbers for READ_LIGHT_SENSOR are based on GPIO numbers"""
+	def __init__(self, pin=4):
+		GPIO.setmode(GPIO.GPIO)
+		GPIO.setwarnings(False)
+		GPIO.setup(pin,GPIO.IN)
+		self.pin = pin
+		
+	def return_data(self, pin):
+		return GPIO.input(self.pin) 
+
+while True:
+	print(READ_LIGHT_SENSOR(pin=17))
+  time.sleep(1)
