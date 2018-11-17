654d9f51fa5b815bb961bbee4d7db663c9a77fd8
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-17 14:33:39 -0500

    Update LCD.py

--- paper/code/LCD.py
+++ paper/code/LCD.py
@@ -6,12 +6,12 @@ from RPi import GPIO
 class LCD_Display(object):
 	"""docstring for LCD_Display"""
 	def __init__(self, cols=16, rows=2, rs=37, e=35, data_pins = [33,31,29,23], mode='BOARD'):
+		GPIO.setwarnings(False)
 		if mode == 'BCM':
 			self.lcd = CharLCD(cols=cols, rows=rows, pin_rs=rs, pin_e=e, pins_data=data_pins, numbering_mode=GPIO.BCM)
 		else:
 			self.lcd = CharLCD(cols=cols, rows=rows, pin_rs=rs, pin_e=e, pins_data=data_pins, numbering_mode=GPIO.BOARD)
-		GPIO.setwarnings(False)
-
+		
 
 	def display_string(self, string=u'Hello World!', clear='N'):
 		if clear == 'Y':
