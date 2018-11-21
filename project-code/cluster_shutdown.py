d9b07da563bff6da9493ffac6402943564024b66
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-21 15:52:05 -0500

    Create cluster_shutdown.py

---
+++ project-code/cluster_shutdown.py
@@ -0,0 +1,13 @@
+#shutdown cluster
+
+from fabric import Connection
+
+parent_node = '10.0.0.31' #wlan0 = 10.0.0.31 | eth0 = 169.254.156.125
+w1 = '169.254.62.205'
+w2 = '169.254.122.189'
+w3 = '169.254.159.1'
+w4 = '169.254.111.219'
+
+for host in ('w1', 'w2', 'w3', 'w4'):
+	result = Connection(host).run('uname -s')
+	print("{}: {}".format(host, result.stdout.strip()))
