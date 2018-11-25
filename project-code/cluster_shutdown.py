bad411341c67144259263980bce29590d0c85dd2
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-25 15:20:31 -0500

    Update cluster_shutdown.py

--- project-code/cluster_shutdown.py
+++ project-code/cluster_shutdown.py
@@ -8,6 +8,17 @@ w2 = '169.254.122.189'
 w3 = '169.254.159.1'
 w4 = '169.254.111.219'
 
+
+workers = {'w1': '169.254.62.205', 'w2': '169.254.122.189', 'w3': '169.254.159.1', 'w4': '169.254.111.219'}
+
+for key, value in workers.items():
+	print(key+': '+value)
+	c = Connection(value)
+	c.connect_kwargs.password = 'raspberry'
+	result = c.run('uname -s')
+	print("{}: {}".format(host, result.stdout.strip()))
+
+'''
 for host in ('w1', 'w2', 'w3', 'w4'):
 	result = Connection(host).run('uname -s')
 	print("{}: {}".format(host, result.stdout.strip()))
