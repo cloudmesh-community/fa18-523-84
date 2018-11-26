b221b33d8e11477b1faf1bcde454d8ce2996d9d5
ahilgenkamp <ahilgie@gmail.com>
2018-11-25 20:17:00 -0500

    updated fabric script

--- project-code/cluster_shutdown.py
+++ project-code/cluster_shutdown.py
@@ -3,11 +3,6 @@
 from fabric import Connection
 
 parent_node = '10.0.0.31' #wlan0 = 10.0.0.31 | eth0 = 169.254.156.125
-w1 = '169.254.62.205'
-w2 = '169.254.122.189'
-w3 = '169.254.159.1'
-w4 = '169.254.111.219'
-
 
 workers = {'w1': '169.254.62.205', 'w2': '169.254.122.189', 'w3': '169.254.159.1', 'w4': '169.254.111.219'}
 
@@ -16,9 +11,4 @@ for key, value in workers.items():
 	c = Connection(value)
 	c.connect_kwargs.password = 'raspberry'
 	result = c.run('uname -s')
-	print("{}: {}".format(host, result.stdout.strip()))
-
-'''
-for host in ('w1', 'w2', 'w3', 'w4'):
-	result = Connection(host).run('uname -s')
-	print("{}: {}".format(host, result.stdout.strip()))
+	print("{}: {}".format(value, result.stdout.strip()))
