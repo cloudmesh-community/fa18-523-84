a5692a5fd0c182f2f8cf9de00d0ae7451bc6077d
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-29 14:26:43 -0500

    Update touch_sensor.py

--- paper/code/touch_sensor.py
+++ paper/code/touch_sensor.py
@@ -3,13 +3,13 @@
 
 import RPi.GPIO as GPIO
 import time
-import os
-import sys
+
 
 class touch_sensor(object):
 	"""docstring for touch_sensor"""
-	def __init__(self, callback_action, pin=7, pin_setup='BOARD'):
+	def __init__(self, function, pin=7, pin_setup='BOARD'):
 		self.pin = pin
+		self.function = function
 		if pin_setup == 'BCM':
 			GPIO.setmode(GPIO.BCM)
 		else:
@@ -17,11 +17,11 @@ class touch_sensor(object):
 		GPIO.setwarnings(False)
 		GPIO.setup(self.pin,GPIO.IN)
 		GPIO.remove_event_detect(self.pin)
-		GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self.callback(callback_action=callback_action))
+		GPIO.add_event_detect(self.pin, GPIO.RISING, callback=lambda x: self.callback(function))
 		
-	def callback(self, channel, callback_action):
+	def callback(self, function):
 		if GPIO.input(self.pin) == 1:
-			callback_action
+			self.function
 		else:
 			pass
 		
@@ -33,7 +33,7 @@ if __name__ == '__main__':
 		def touch_print(text):
 			print(text)
 
-		touch_sensor(callback_action=touch_print('Hey!'), pin=13)
+		touch_sensor(pin=13, function=touch_print('Hey!'))
 		while True:
 			time.sleep(1)		
 	except KeyboardInterrupt:
