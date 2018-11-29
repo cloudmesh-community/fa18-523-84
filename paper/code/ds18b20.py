8d3395eee51d958c18b25a759f07cefd3fb47eee
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-29 13:03:53 -0500

    Update ds18b20.py

--- paper/code/ds18b20.py
+++ paper/code/ds18b20.py
@@ -7,7 +7,7 @@ import time
 
 class ds18b20(object):
 	"""docstring for ds18b20
-		DS18B20 sensor should be plugged into GPIO4
+	DS18B20 sensor should be plugged into GPIO4
 	"""
 	def __init__(self):
 		os.system('modprobe w1-gpio')
@@ -17,31 +17,31 @@ class ds18b20(object):
 		self.device_file = self.device_folder + '/w1_slave'
 
 	def read_temp_raw(self):
-	    f = open(self.device_file, 'r')
-	    lines = f.readlines()
-	    f.close()
-	    return lines
+		f = open(self.device_file, 'r')
+		lines = f.readlines()
+		f.close()
+		return lines
 
 
 	def read_temp(self):
-	    lines = self.read_temp_raw()
-	    while lines[0].strip()[-3:] != 'YES':
-	        time.sleep(0.2)
-	        lines = read_temp_raw()
-	    equals_pos = lines[1].find('t=')
-	    if equals_pos != -1:
-	        temp_string = lines[1][equals_pos+2:]
-	        temp_c = float(temp_string) / 1000.0
-	        temp_f = temp_c * 9.0 / 5.0 + 32.0
-	        return temp_c, temp_f
-  	
+		lines = self.read_temp_raw()
+		while lines[0].strip()[-3:] != 'YES':
+			time.sleep(0.2)
+			lines = read_temp_raw()
+		equals_pos = lines[1].find('t=')
+		if equals_pos != -1:
+			temp_string = lines[1][equals_pos+2:]
+			temp_c = float(temp_string) / 1000.0
+			temp_f = temp_c * 9.0 / 5.0 + 32.0
+			return temp_c, temp_f
+
 if __name__ == '__main__':
-    try:
-	print(ds18b20().read_temp())  
-	time.sleep(1)
-    except KeyboardInterrupt:
-        print('Stopping Program')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+	try:
+		print(ds18b20().read_temp())
+		time.sleep(1)
+	except KeyboardInterrupt:
+		print('Stopping Program')
+		try:
+			sys.exit(0)
+		except SystemExit:
+			os._exit(0)
