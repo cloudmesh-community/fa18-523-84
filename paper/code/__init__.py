1afb5b4bb3e746737d6b8a5f999cc181ffc577e0
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-11-30 12:48:14 -0500

    Create __init__.py

---
+++ paper/code/__init__.py
@@ -0,0 +1,11 @@
+# allows to say
+# import paper.code
+# from paper.code import *
+# from paper.code import LCD
+
+from .LCD import LCD 
+from .light_sensor import light_sensor
+from .relay_switch import relay_switch
+from .ds18b20 import ds18b20
+from .temp_humid import temp_humid
+from .touch_sensor import touch_sensor
