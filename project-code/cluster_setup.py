a5636ab12c868ea74b351ecfe7b3f3ca0d688a21
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-12-02 12:17:31 -0500

    Create cluster_setup.py

---
+++ project-code/cluster_setup.py
@@ -0,0 +1,12 @@
+#Initial Cluster Setup
+
+from fabric import Connection
+
+workers = {'w1': '169.254.62.205', 'w2': '169.254.122.189', 'w3': '169.254.159.1', 'w4': '169.254.111.219'}
+
+for key, value in workers.items():
+	print(key+': '+value)
+	c = Connection(value)
+	c.connect_kwargs.password = 'raspberry'
+	result = c.run('sudo echo pi:Weather!Center01 | chpsswd') #uname -s
+	print("{}: {}".format(value, result.stdout.strip()))
