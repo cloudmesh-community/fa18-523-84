a03fb83dedf1c7e9825bda274f78a700b2b6c124
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-12-02 14:47:03 -0500

    Update cluster_shutdown.py

--- project-code/cluster_shutdown.py
+++ project-code/cluster_shutdown.py
@@ -9,6 +9,6 @@ workers = {'w1': '169.254.62.205', 'w2': '169.254.122.189', 'w3': '169.254.159.1
 for key, value in workers.items():
 	print(key+': '+value)
 	c = Connection(value)
-	c.connect_kwargs.password = 'raspberry'
+	c.connect_kwargs.password = 'Weather_Center01' #change back to raspberry
 	result = c.run('sudo shutdown -h now') #uname -s
 	print("{}: {}".format(value, result.stdout.strip()))
