fc232f835dc4cc18bc6de7dde0bbd9ad0bc0900a
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-12-02 14:43:16 -0500

    Update cluster_setup.py

--- project-code/cluster_setup.py
+++ project-code/cluster_setup.py
@@ -7,6 +7,31 @@ workers = {'PiCluster_w01': '169.254.62.205',
 	   'PiCluster_w03': '169.254.159.1', 
 	   'PiCluster_w04': '169.254.111.219'}
 
+@needs_host
+def reboot(wait=120, command='reboot', use_sudo=True):
+	# Shorter timeout for a more granular cycle than the default.
+	timeout = 5
+	# Use 'wait' as max total wait time
+	attempts = int(round(float(wait) / float(timeout)))
+	# Don't bleed settings, since this is supposed to be self-contained.
+	# User adaptations will probably want to drop the "with settings()" and
+	# just have globally set timeout/attempts values.
+	with settings(
+		hide('running'),
+		timeout=timeout,
+		connection_attempts=attempts
+	):
+		(sudo if use_sudo else run)(command)
+		# Try to make sure we don't slip in before pre-reboot lockdown
+		time.sleep(5)
+		# This is actually an internal-ish API call, but users can simply drop
+		# it in real fabfile use -- the next run/sudo/put/get/etc call will
+		# automatically trigger a reconnect.
+		# We use it here to force the reconnect while this function is still in
+		# control and has the above timeout settings enabled.
+		connections.connect(env.host_string)
+
+
 for key, value in workers.items():
 	print(key+': '+value)
 	c = Connection(value, connect_timeout=60)
@@ -15,6 +40,5 @@ for key, value in workers.items():
 	#print('password changed')
 	#c.run('sudo hostnamectl set-hostname '+key)
 	#print('hostname changed')
-	c.run('sudo reboot')
+	reboot()
 	print('rebooting '+key)
-	time.sleep(30)
