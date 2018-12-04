8f9ba32b1acaaa59fb2a6831003cb376785a51c9
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-12-03 23:00:16 -0500

    Update parent_node.sh

--- project-code/parent_node.sh
+++ project-code/parent_node.sh
@@ -8,13 +8,13 @@ sudo apt-get dist-upgrade -y
 echo "INFO: updates complete"
 
 #install nmap for networking
-sudo apt-get install nmap
+sudo apt-get install nmap -y
 #install and set up apache web server
 #sources: https://www.youtube.com/watch?v=iVGtJOC71Fw
-sudo apt-get install apache2
-sudo apt-get remove libapache2-mod-python libapache2-mod-wsgi
-sudo apt-get install libapache2-mod-wsgi-py3
-sudo a2enmod wsgi
+sudo apt-get install apache2 -y
+sudo apt-get remove libapache2-mod-python libapache2-mod-wsgi -y
+sudo apt-get install libapache2-mod-wsgi-py3 -y
+sudo a2enmod wsgi -y
 
 echo "INFO: tools and dependancies installed"
 
@@ -27,5 +27,13 @@ pip3 install flask
 pip3 install wtforms
 
 echo "INFO: python packages successfully installed"
+
+#install cassandra on the parent node
+#Sources: https://www.linode.com/docs/databases/cassandra/deploy-scalable-cassandra/
+sudo apt install software-properties-common -y
+sudo apt-get purge openjdk*
+sudo apt-get install openjdk-8-jdk -y
+java -version
+sudo apt-get install ntp -y
 
 
