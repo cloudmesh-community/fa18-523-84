3650e12b2fc825e33358c89c066f79a4fec46707
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-12-04 09:35:55 -0500

    Update cluster_setup.py

--- project-code/cluster_setup.py
+++ project-code/cluster_setup.py
@@ -8,7 +8,7 @@ workers = {'PiCluster_w01': '169.254.62.205',
 	   'PiCluster_w04': '169.254.111.219'}
 
 for key, value in workers.items():
-	print(key+': '+value)
+	#print(key+': '+value)
 	c = Connection(value, connect_timeout=60)
 	c.connect_kwargs.password = 'Weather_Center01' #change back to raspberry
 	
@@ -37,6 +37,11 @@ for key, value in workers.items():
 	print('INFO: upgrades complete')
 	'''
 	
+	#Install packages and programs
+	c.run('sudo apt-get install python3-pip')
+	c.run('sudo apt-get install python3-pandas')
+	print('INFO: python packages installed')
+	
 	#reboot each node
-	c.run('sudo shutdown -r 1') #reboot in 60 sec to avoid issues with ssh connection
+	#c.run('sudo shutdown -r 1') #reboot in 60 sec to avoid issues with ssh connection
 	
