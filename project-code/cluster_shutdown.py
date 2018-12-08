7e23cecd86f129692cf0b659950f223c12655f86
ahilgenkamp <39466667+ahilgenkamp@users.noreply.github.com>
2018-12-07 19:41:40 -0500

    Update cluster_shutdown.py

--- project-code/cluster_shutdown.py
+++ project-code/cluster_shutdown.py
@@ -5,13 +5,17 @@
 
 from fabric import Connection
 
-parent_node = '10.0.0.31' #wlan0 = 10.0.0.31 | eth0 = 169.254.156.125
-
-workers = {'w1': '169.254.62.205', 'w2': '169.254.122.189', 'w3': '169.254.159.1', 'w4': '169.254.111.219'}
+workers = {
+	'PiCluster_p01': '10.0.0.42',
+	'PiCluster_w01': '10.0.0.36',
+	'PiCluster_w02': '10.0.0.37',
+	'PiCluster_w03': '10.0.0.41',
+	'PiCluster_w04': '10.0.0.40'
+	}
 
 for key, value in workers.items():
 	print(key+': '+value)
 	c = Connection(value)
 	c.connect_kwargs.password = 'Weather_Center01' #change back to raspberry
-	result = c.run('sudo shutdown -h 1') #uname -s
+	result = c.run('sudo shutdown -h 1')
 	print("{}: {}".format(value, result.stdout.strip()))
